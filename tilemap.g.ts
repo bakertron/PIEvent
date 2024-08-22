2ab427acffca6373559861a3e74c6c509cc1bcd9
Don Locke <75755573+DonLocke@users.noreply.github.com>
2024-08-22 13:18:23 -0600

    Add more Carpet

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,15 +3,11 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile6 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -119,16 +115,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile1":
-            case "tile3":return tile3;
-            case "myTile2":
-            case "tile4":return tile4;
-            case "myTile4":
-            case "tile6":return tile6;
             case "CarpetTile":
             case "tile1":return tile1;
             case "Desk":
             case "tile2":return tile2;
+            case "Carpet2":
+            case "tile3":return tile3;
         }
         return null;
     })
